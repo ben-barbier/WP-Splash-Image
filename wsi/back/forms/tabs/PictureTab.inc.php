f6a9a3fe614919855df077408401a9e50849597a
Benjamin Barbier <benjamin.barbier@dark-sides.com>
2012-01-11 17:33:42 +0100

    Add tabs DAO management

--- wsi/back/forms/tabs/PictureTab.inc.php
+++ wsi/back/forms/tabs/PictureTab.inc.php
@@ -1,14 +1,14 @@
 <div id="tab_picture">
 	<table id="box_picture" class="box_type">
 		<tr>
-			<td rowspan="4"><input type="radio" id="radio_picture" name="wsi_type" value="picture" <?php if(get_option('wsi_type')=="picture") echo('checked="checked"') ?> /></td>
+			<td rowspan="4"><input type="radio" id="radio_picture" name="wsi_type" value="picture" <?php if($siBean->getWsi_type()=="picture") echo('checked="checked"') ?> /></td>
 			<td><span><?php echo __("Picture URL:",'wp-splash-image'); ?></span></td>
 			<td><input 
 				type="text" 
 				name="url_splash_image"
 				id="url_splash_image"
 				size="80" 
-				value="<?php echo esc_attr(get_option('url_splash_image')); ?>" /></td>
+				value="<?php echo $siBean->getUrl_splash_image(); ?>" /></td>
 		</tr>
 		<tr>
 			<td><span><?php echo __("Picture link URL",'wp-splash-image'); ?>:</span></td>
@@ -16,13 +16,13 @@
 				type="text" 
 				name="wsi_picture_link_url" 
 				size="50" 
-				value="<?php echo esc_attr(get_option('wsi_picture_link_url')); ?>" />
+				value="<?php echo $siBean->getWsi_picture_link_url(); ?>" />
 				<?php echo __('(stay empty if not required)','wp-splash-image'); ?></td>
 		</tr>
 		<tr>
 			<td><span><?php echo __("Picture link target",'wp-splash-image'); ?>:</span></td>
 			<td>
-				<select name="wsi_picture_link_target" value="<?php echo esc_attr(get_option('wsi_picture_link_target')); ?>">
+				<select name="wsi_picture_link_target" value="<?php echo $siBean->getWsi_picture_link_target(); ?>">
 					<option value="self">Self</option>
 					<option value="blank">Blank</option>
 				</select>
