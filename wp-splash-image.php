faa3e5813779161b548bb34bb4d890e1256c64c6
Benjamin Barbier <benjamin.barbier@dark-sides.com>
2012-01-30 02:16:39 +0100

    Include Config Bean

--- wp-splash-image.php
+++ wp-splash-image.php
@@ -9,6 +9,7 @@ Author URI: http://www.dark-sides.com/
 Donate URI: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=CKGNM6TBHU72C
 */
 
+include 'wsi/beans/ConfigBean.class.php';
 include 'wsi/beans/SplashImageBean.class.php';
 include 'wsi/DAO/MainManager.class.php';
 include 'wsi/DAO/ConfigManager.class.php';
