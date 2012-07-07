d2ea28b8d6394da5440909285474afd65a84fe31
Benjamin Barbier <benjamin.barbier@dark-sides.com>
2012-07-07 15:59:19 +0200

    MAJ du front (récupération des paramètres dans le bean)

--- wsi/front/splash/overlay.inc.php
+++ wsi/front/splash/overlay.inc.php
@@ -3,8 +3,8 @@
 $j(document).ready(function () {
 	$j("#splashLink").overlay({
 		mask: {
-			color: '#<?php echo get_option('splash_color'); ?>',
-			opacity: <?php echo (get_option('wsi_opacity')/100); ?> 
+			color: '#<?php echo $siBean->getSplash_color(); ?>',
+			opacity: <?php echo ($siBean->getWsi_opacity()/100); ?> 
 		},
 		load: true // Lance la Splash Image à l'ouverture			
 		<?php if ($siBean->isWsi_close_esc_function()=='true') { echo(',closeOnClick: false'); } ?>
