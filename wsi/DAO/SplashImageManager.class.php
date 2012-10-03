c32215db0b8da9a214c843be484b8cf5383e524c
Benjamin Barbier <benjamin.barbier@dark-sides.com>
2012-10-03 22:04:41 +0300

    Add field on admin page

--- wsi/DAO/SplashImageManager.class.php
+++ wsi/DAO/SplashImageManager.class.php
@@ -58,13 +58,14 @@ class SplashImageManager {
 						'datepicker_end'          => $splashImageBean->getDatepicker_end(),
 
 						// Booleans management
-						'wsi_close_esc_function'    => (($splashImageBean->isWsi_close_esc_function())?'1':'0'),
-						'wsi_hide_cross'            => (($splashImageBean->isWsi_hide_cross())?'1':'0'),
-						'wsi_disable_shadow_border' => (($splashImageBean->isWsi_disable_shadow_border())?'1':'0'),
-						'wsi_youtube_autoplay'      => (($splashImageBean->isWsi_youtube_autoplay())?'1':'0'),
-						'wsi_youtube_loop'          => (($splashImageBean->isWsi_youtube_loop())?'1':'0'),
-						'wsi_fixed_splash'          => (($splashImageBean->isWsi_fixed_splash())?'1':'0'),
-						'wsi_display_always'        => (($splashImageBean->isWsi_display_always())?'1':'0'),
+						'wsi_close_esc_function'     => (($splashImageBean->isWsi_close_esc_function())?'1':'0'),
+						'wsi_hide_cross'             => (($splashImageBean->isWsi_hide_cross())?'1':'0'),
+						'wsi_disable_shadow_border'  => (($splashImageBean->isWsi_disable_shadow_border())?'1':'0'),
+						'wsi_youtube_autoplay'       => (($splashImageBean->isWsi_youtube_autoplay())?'1':'0'),
+						'wsi_youtube_loop'           => (($splashImageBean->isWsi_youtube_loop())?'1':'0'),
+						'wsi_fixed_splash'           => (($splashImageBean->isWsi_fixed_splash())?'1':'0'),
+						'wsi_display_always'         => (($splashImageBean->isWsi_display_always())?'1':'0'),
+						'wsi_hide_on_mobile_devices' => (($splashImageBean->isWsi_hide_on_mobile_devices())?'1':'0'),
 						
 						// Tabs values
 						'wsi_youtube'     => $splashImageBean->getWsi_youtube(),
@@ -135,6 +136,7 @@ class SplashImageManager {
 			$splashImageBean->setWsi_youtube_loop(           esc_attr($wsi_splashimage_row->wsi_youtube_loop=='1'?'true':'false'));
 			$splashImageBean->setWsi_fixed_splash(           esc_attr($wsi_splashimage_row->wsi_fixed_splash=='1'?'true':'false'));
 			$splashImageBean->setWsi_display_always(         esc_attr($wsi_splashimage_row->wsi_display_always=='1'?'true':'false'));
+			$splashImageBean->setWsi_hide_on_mobile_devices( esc_attr($wsi_splashimage_row->wsi_hide_on_mobile_devices=='1'?'true':'false'));
 
 			// Valeurs des onglets
 			$splashImageBean->setWsi_youtube(                esc_attr($wsi_splashimage_row->wsi_youtube));
@@ -160,6 +162,7 @@ class SplashImageManager {
 		return array(
 				'id'                         => 1,
 				'wsi_display_always'         => 0, // false
+				'wsi_hide_on_mobile_devices' => 0, // false
 				'wsi_idle_time'              => '30',
 				'url_splash_image'           => 'http://plugins.svn.wordpress.org/wsi/assets/banner-772x250.png',
 				'splash_image_width'         => '772',
@@ -225,6 +228,7 @@ class SplashImageManager {
 		$result.= "<strong>".$this->tableName().": </strong><br />";
 		$result.= "id: ".                        $this->splashImageBean->getId()."<br />";
 		$result.= "wsi_display_always: ".        $this->splashImageBean->isWsi_display_always()."<br />";
+		$result.= "wsi_hide_on_mobile_devices: ".$this->splashImageBean->isWsi_hide_on_mobile_devices()."<br />";
 		$result.= "wsi_idle_time: ".             $this->splashImageBean->getWsi_idle_time()."<br />";
 		$result.= "url_splash_image: ".          $this->splashImageBean->getUrl_splash_image()."<br />";
 		$result.= "splash_image_width: ".        $this->splashImageBean->getSplash_image_width()."<br />";
